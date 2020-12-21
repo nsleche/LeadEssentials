b6d1bee64d7b283a465a472be7a99bb4ce5aa85e
nschucky <antonioalvesdev@gmail.com>
2020-12-20 23:44:59 -0800

    replace hardcoded JSON with helper method

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -65,7 +65,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
         let (sut, client) = makeSUT()
         expect(sut, toCompleteWithResult: .success([])) {
-            let emptyJSONData = Data("{\"items\": []}".utf8)
+            let emptyJSONData = makeItemsJSON([])
             client.complete(withStatusCode: 200, data: emptyJSONData)
         }
     }
