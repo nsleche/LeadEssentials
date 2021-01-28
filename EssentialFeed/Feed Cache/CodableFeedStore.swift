68699667b7841ad4e6cd35ea3669a71e5c0d057b
nschucky <antonioalvesdev@gmail.com>
2021-01-27 23:35:24 -0800

    GO BACK TO ***CODABLEFEEDSTORE*** IN THE FUTURE, I COULDNT MAKE IT WORK WITH THROWS FUNCTIONS INSTEAD OF COMPLETIONS

--- EssentialFeed/Feed Cache/CodableFeedStore.swift
+++ EssentialFeed/Feed Cache/CodableFeedStore.swift
@@ -43,41 +43,101 @@ public class CodableFeedStore: FeedStore {
     public init(storeURL: URL) {
         self.storeURL = storeURL
     }
+
     
-    
-    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        let storeURL = self.storeURL
-        queue.async(flags: .barrier) {
-            do {
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date) throws {
+        let url = self.storeURL
+        
+        try performSync {
+            Result {
                 let encoder = JSONEncoder()
                 let codableFeedImage = feed.map { CodableFeedImage(feedImage: $0) }
                 let encoded = try encoder.encode(Cache(feed: codableFeedImage, timestamp: timestamp))
-                try encoded.write(to: storeURL)
-                completion(nil)
-            } catch {
-                completion(error)
+                try encoded.write(to: url)
             }
         }
     }
     
-    public func retrieve(completion: @escaping RetrieveCompletion) {
-        let storeURL = self.storeURL
+    private func insertSync(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping (Error?) -> Void) {
+        let url = self.storeURL
+        
+        queue.async(flags: .barrier) {
+            let encoder = JSONEncoder()
+            let codableFeedImage = feed.map { CodableFeedImage(feedImage: $0) }
+            guard let encoded = try? encoder.encode(Cache(feed: codableFeedImage, timestamp: timestamp)) else {
+                return completion(nil)
+            }
+            try? encoded.write(to: url)
+            completion(nil)
+        }
+        
+    }
+    
+    private func execute(completion: @escaping (CachedFeed?) -> Void) {
+        let url = self.storeURL
+        
         queue.async {
-            guard let data = try? Data(contentsOf: storeURL) else {
-                return completion(.empty)
+            guard let data = try? Data(contentsOf: url) else {
+                return completion(nil)
             }
             
-            do {
-                let decoder = JSONDecoder()
-                let cache = try decoder.decode(Cache.self, from: data)
-                completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
-            } catch {
-                completion(.failure(error))
+            let decoder = JSONDecoder()
+            guard let cache = try? decoder.decode(Cache.self, from: data) else {
+                return completion(nil)
             }
+            completion(CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp))
         }
     }
     
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+    public func retrieve() throws -> CachedFeed? {
+        let url = self.storeURL
+        return try performSync {
+            Result {
+                try CodableFeedStore.find(in: url).map {
+                    CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
+                }
+            }
+        }
+
+        
+    }
+    
+
+    
+    
+    func performSync<R>(_ action: @escaping () -> Result<R, Error>) throws -> R {
+        let url = self.storeURL
+        var result: Result<R, Error>!
+        
+        queue.async(flags: .barrier) {
+            result = action()
+        }
+        return try result.get()
+    }
+    
+    
+    public func deleteCachedFeed() throws {
+        
+        deleteSynce { (error) in
+            print("error deleting")
+        }
+        
+//        try performSync { storeURL in
+//            Result {
+//                guard FileManager.default.fileExists(atPath: storeURL.path) else {
+//                    throw NSError(domain: "FileDoesNotExist", code: 90990, userInfo: nil)
+//                }
+//
+//                do {
+//                    try FileManager.default.removeItem(at: storeURL)
+//                } catch {
+//                    throw NSError(domain: "ErrorRemovingItem", code: 90990, userInfo: nil)
+//                }
+//            }
+//        }
+    }
+    
+    public func deleteSynce(completion: @escaping (Error?) -> Void) {
         let storeURL = self.storeURL
         queue.async(flags: .barrier) {
             guard FileManager.default.fileExists(atPath: storeURL.path) else {
@@ -88,10 +148,22 @@ public class CodableFeedStore: FeedStore {
                 try FileManager.default.removeItem(at: storeURL)
                 completion(nil)
             } catch {
-                completion(error)
+                completion(nil)
             }
         }
     }
+    
 }
 
-*/
+extension CodableFeedStore {
+    static private func find(in url: URL) throws -> Cache? {
+        let data = try Data(contentsOf: url)
+        
+        let decoder = JSONDecoder()
+        return try decoder.decode(Cache.self, from: data)
+        //completion(CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp))
+    }
+}
+
+ */
+
