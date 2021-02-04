8ead6d3727345697a4bfe8976db95a473d0727d2
nschucky <antonioalvesdev@gmail.com>
2021-02-03 22:31:39 -0800

    Add FeedViewController test file

---
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  FeedViewControllerTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Antonio Alves on 2/3/21.
+//
+
+import XCTest
+
+class FeedViewControllerTests: XCTestCase {
+    
+    func test_init_doesNotLoadFeed() {
+        
+    }
+}
