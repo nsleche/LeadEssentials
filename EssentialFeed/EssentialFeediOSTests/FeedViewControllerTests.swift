550960dd6373563847d663dc12918a70c4ba9abe
nschucky <antonioalvesdev@gmail.com>
2021-02-04 10:46:01 -0800

    Add spy classes

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -7,9 +7,30 @@
 
 import XCTest
 
+final class FeedViewController {
+    
+    let loader: FeedViewControllerTests.LoaderSpy
+    init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.loader = loader
+    }
+    
+}
+
 class FeedViewControllerTests: XCTestCase {
     
     func test_init_doesNotLoadFeed() {
-        
+        let sut = makeSUT()
+        XCTAssertEqual(sut.loader.loadAllCount, 0)
+    }
+    
+    
+    private func makeSUT() -> FeedViewController {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        return sut
+    }
+    
+    class LoaderSpy {
+        private(set) var loadAllCount: Int = 0
     }
 }
